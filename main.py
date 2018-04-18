bc2ae14c7780ea2b89ec6589f635dfdf34a6276c
Hien Vu <hienn.vu11@gmail.com>
2018-04-19 09:54:47 +1000

    first-commit

---
+++ main.py
@@ -0,0 +1,16 @@
+from find_eye import *
+import argparse
+
+########
+import math
+########
+
+# construct the argument parse and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", help = "path to the image file")
+args = vars(ap.parse_args())
+
+# load the image
+image = cv2.imread(args["image"])
+
+contours = find_eye(image)

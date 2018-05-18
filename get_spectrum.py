9f7cc0822a8d818479ad6a4a1aad14e92408a7ab
Hien Vu <hienn.vu11@gmail.com>
2018-05-18 14:35:14 +1000

    Calibrated spectrum using 500nm, standardised to %, plot using matplotlib

--- get_spectrum.py
+++ get_spectrum.py
@@ -7,38 +7,102 @@ import numpy as np
 import cv2
 
 
+# only get the most relevant parts of the spectrum
+def calibrate(spec, green500):
+	# calibrate using points at 0nm (the point source) and 500nm
+	wav = []
+	intensities = []
+	# calculate the conversion
+	conv = green500/500
+	# for each wavelength, find the associated pixel and intensity
+	for w in range(400,721):
+		pix = round(w*conv)
+		adjust = spec[0][0]
+		i = spec[pix-adjust][1]
+		intensities.append(i)
+		wav.append([w, i])
+	# standardise to percentages
+	max_intensity = max(intensities)
+	for val in wav:
+		val[1] = val[1]/max_intensity
+	# smoothing
+	
+	return wav
+
+# convert RGB to HSV and return hue
+def get_hue(colour):
+	rr,gg,bb = colour
+	r,g,b = rr/255, gg/255, bb/255
+	M = max(r,g,b)
+	m = min(r,g,b)
+	c = M - m
+	if c == 0:
+		hue = 0
+	elif M == r:
+		#g, b = int(round(g)), int(round(b))
+		hue = ((g-b)/c)%6
+	elif M == g:
+		hue = (b-r)/c + 2
+	elif M == b:
+		hue = (r-g)/c + 4
+	hue = hue*60
+	return hue
+
+
+UPPERBOUND = 0
+LOWERBOUND = 1300
 # Returns the spectrum (pixel v intensity) for an eye given the left and right boundaries
 # Intensity is total intensity all pixels in that row
-# Image must be grayscale
 def eye_spectrum(eye, left, right, image):
+	gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 	spectrum = []
+	rgb = []
 	[x, y, extL, extR, extT, extB] = eye
-#	numpix = right - left
 	# for each row in the image until just above the top of the eye
-	for i in range(0, extT[1]-(extB[1]-extT[1])):
-		intensity = 0
+	####### CHANGE RANGE VALUES AFTER CALIBRATION - CAN BE CENTRE OF EYE + KNOWN VALUE TO AVOID NOISE
+	for i in range(UPPERBOUND, LOWERBOUND):  # extT[1]-(extB[1]-extT[1])
+		numpix, intensity, tot_r, tot_g, tot_b = 0, 0, 0, 0, 0
 		# for each pixel in section containing spectrum
 		for j in range(left, right):
 			# if pixel is bright enough
-			if image[i][j] > 10:
-				# get brightness imagegray[i][j]
-				intensity += image[i][j]
-#		# average out the brightness for the row
-#		ave_intensity = intensity / numpix
-		spectrum.append([i, intensity])
-	return spectrum
+			if gray[i][j] > 10:
+				numpix += 1
+				# get brightness
+				intensity += gray[i][j]
+				b,g,r = image[i,j]
+				tot_r += r
+				tot_g += g
+				tot_b += b
+		# spectrum is indexed from 0 at the centre of the eye
+		spectrum.insert(0, [y-i, intensity])
+		if numpix != 0:
+			fin_r, fin_g, fin_b = tot_r/numpix, tot_g/numpix, tot_b/numpix
+		else:
+			fin_r, fin_g, fin_b = 0, 0, 0
+		rgb.insert(0, [fin_r, fin_g, fin_b])
+	# get hue from rgb
+	H = []
+	for colour in rgb:
+		hue = get_hue(colour)
+		H.append(hue)
+	# wave 500nm = 154 degrees hue
+	# find the row pertaining to that green
+	greenidx = (np.abs(np.asarray(H)-154)).argmin()
+	green500 = spectrum[greenidx][0]
+	# calibrate each rgb with wavelength using green
+	final_spec = calibrate(spectrum, green500)
+	return final_spec
 
 
 # Calls eye_spectrum() for each eye
 	# pair = [eye1, eye2]
 	# eye1 = [x, y, extL, extR, extT, extB]
 def get_spectrum(pair, image):
-	gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 	eye1, eye2 = pair
-	
+
 	width1, width2 = eye1[3][0]-eye1[2][0], eye2[3][0]-eye2[2][0]
 	cen = math.floor( (eye1[0]+eye2[0]) / 2 )
 
-	spec1 = eye_spectrum(eye1, math.floor(eye1[2][0]-width1), cen, gray)
-	spec2 = eye_spectrum(eye2, cen, math.ceil(eye2[3][0]+width2)+1, gray)
+	spec1 = eye_spectrum(eye1, math.floor(eye1[2][0]-width1), cen, image)
+	spec2 = eye_spectrum(eye2, cen, math.ceil(eye2[3][0]+width2)+1, image)
 	return [spec1, spec2]
--- main.py
+++ main.py
@@ -1,24 +1,52 @@
+# Locates the eyes and extracts data from images, one normal and one with spectrum
+# Adds eye to training database with label
+
 from find_eye import *
 from find_pairs import *
 from get_colour import *
 from get_spectrum import *
 import argparse
-import pprint
-
+import matplotlib.pyplot as plt
+import csv
+import os
 
 # construct the argument parse and parse the arguments
 ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--image", help = "path to the image file")
+ap.add_argument("-i", "--image", help = "path to the normal image file")
+ap.add_argument("-c", "--class", help = "class for training data")
+# ap.add_argument("-s", "--splitimage", help = "path to the spectrum image file")
 args = vars(ap.parse_args())
 
 # load the image
 image = cv2.imread(args["image"])
 orig = image.copy()
-contours = find_eye(image)
 
+# for training data
+ID = args["class"]
+
+# find pairs of eyes
+contours = find_eye(image)
 [con_pairs, pair_det] = find_pairs(image, contours)
 num_pairs = len(con_pairs)
-print("Found " + str(num_pairs) + " pair/s")
+fname = os.path.basename(args["image"])
+print("------- RESULTS -------")
+print("SEARCHED " + str(fname))
+print("FOUND " + str(num_pairs) + " PAIR/S")
+
+# set up new databases
+# interpupillary distance and colour
+fields1 = ['file','ID','distance','hue','r','g','b']
+f1 = open("distcolour.csv", 'w')
+writer = csv.writer(f1)
+writer.writerow(fields1)
+f1.close()
+# spectrum
+fields2 = ['file','ID','L/R']
+fields2 = fields2 + list(range(400, 721))
+f2 = open("spec.csv", 'w')
+writer = csv.writer(f2)
+writer.writerow(fields2)
+f2.close()
 
 # For each pair
 for i in range(0, num_pairs):
@@ -36,18 +64,53 @@ for i in range(0, num_pairs):
 	col1 = get_colour(orig, eye1[0:2], ave_w/2)
 	col2 = get_colour(orig, eye2[0:2], ave_w/2)
 	ave_col = ave_eye_colours(col1, col2)
-
+	r,g,b = ave_col
+	hue = get_hue(ave_col)
+	
+	# print results
 	print("---------")
 	print("NAE Pair " + str(i+1))
 	print("Interpupillary distance: " + str(dist))
+	print("Colour (hue): " + str(hue))
 	print("Colour (RGB): " + str(ave_col))
+	
+	# add interpupillary distance and colour to database
+	f1 = open("distcolour.csv", 'a')
+	writer = csv.writer(f1)
+	writer.writerow([fname,ID, dist, hue, r, g, b])
+	f1.close()
+
+	# get intensity
+
+	# get label
 
 	# get spectrum
 	[spec1, spec2] = get_spectrum(pair, orig)
-	print("Left eye spectrum")
-	print(spec1)
-	print("Right eye spectrum")
-	print(spec2)
+
+	# graph spectrum
+	x1, y1 = zip(*spec1)
+	plt.scatter(x1,y1)
+
+	x2, y2 = zip(*spec2)
+	plt.figure()
+	plt.scatter(x2,y2)
+	plt.show()
+	print(list(y1))
+	# add spectrum to spec database
+	f2 = open("spec.csv", 'a')
+	writer = csv.writer(f2)
+	writer.writerow([fname,ID,'L']+list(y1))
+	writer.writerow([fname,ID,'R']+list(y2))
+	f2.close()
+	#print("Left eye spectrum")
+	#print(spec1)
+	#print("Right eye spectrum")
+	#print(spec2)
+
+
+	
+	# add to database
+
 
 
 
